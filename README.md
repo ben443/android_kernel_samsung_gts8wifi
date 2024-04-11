32142de4cb47d4e3fb5936a0cf61fe96e3ca33a4
InfiniteVariations1 <129690844+InfiniteVariations1@users.noreply.github.com>
2024-04-11 15:33:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# android_kernel_samsung_gts8wifi
+ Kernel souce for Galaxy Tab S8 Wifi.
---
+++ kernel_platform/common/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Apr 11 14:52:25 CDT 2024"
+#define LINUX_COMPILE_BY "archvisions"
+#define LINUX_COMPILE_HOST "pop-os"
+#define LINUX_COMPILER "gcc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0, GNU ld (GNU Binutils for Ubuntu) 2.38"
