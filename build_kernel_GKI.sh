daef4fd6b990635546d7b5174369935596e4aef7
Ben graham <64079735+ben443@users.noreply.github.com>
2025-07-15 23:18:12 +1000

    Update build_kernel_GKI.sh

--- build_kernel_GKI.sh
+++ build_kernel_GKI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #1. target config
-BUILD_TARGET=gts8_kor_openx
+BUILD_TARGET=gts8wifi_eur_open
 export MODEL=$(echo $BUILD_TARGET | cut -d'_' -f1)
 export PROJECT_NAME=${MODEL}
 export REGION=$(echo $BUILD_TARGET | cut -d'_' -f2)
